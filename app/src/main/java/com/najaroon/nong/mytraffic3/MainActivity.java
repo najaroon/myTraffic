58c9aa4477cac42e31eb7249acae444a6ef6aa44
kraipon <nctc3fa.kraipon@gmail.com>
2016-06-04 15:02:49 +0700

    Finish 4 June 2016

--- app/src/main/java/com/najaroon/nong/mytraffic3/MainActivity.java
+++ app/src/main/java/com/najaroon/nong/mytraffic3/MainActivity.java
@@ -1,18 +1,46 @@
 package com.najaroon.nong.mytraffic3;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Bind Widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+        // Controller Button
+        controllerButton();
 
     }   // Main Method
 
+    private void controllerButton() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cat);
+                mediaPlayer.start();
+            } // onClick
+        });
+
+    }// ControllerButton
 
 
 }   //Main Class
